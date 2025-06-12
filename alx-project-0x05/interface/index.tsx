94aaddc9aaa4cbfcffb886a581754b3e96afa497
Muturi1999 <mikemuturi1999@gmail.com>
2025-06-12 10:14:23 +0300

    updated context/CountContext.tsx, components/layout/Header.tsx, pages/_app.tsx

---
+++ alx-project-0x05/interface/index.tsx
@@ -0,0 +1,14 @@
+export interface PageRouteProps {
+  pageRoute: string;
+}
+
+export interface ButtonProps {
+  action: () => void;
+  buttonLabel: string;
+  buttonSize?: string
+  buttonBackgroundColor: string;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
